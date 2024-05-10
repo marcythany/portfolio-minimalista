ac8020663f2113b32902ffd6db12440fd516715d
Marcy Thany <marcythany@gmail.com>
2024-05-09 21:44:53 -0300

    feat: implement persistent theme selection for portfolio website
    This commit introduces a more robust theme selection logic for the portfolio website. Key improvements include:
    
    * Storing user's theme preference (light/dark) in localStorage for persistence across sessions.
    * Checking for stored theme preference before defaulting to user's color scheme.
    * Applying the appropriate theme class to the document based on the chosen theme.
    
    These changes ensure consistent theme behavior regardless of the user's initial color scheme preference and allow them to set a preferred theme that persists across sessions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcy-miniportfolio.vercel.app/',
+  output: 'server',
+  adapter: vercel(),
 })
