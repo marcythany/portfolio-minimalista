7c7c95ec0fe8da53cac638edbe66989aa39023b9
Marcy Thany <marcythany@gmail.com>
2024-05-12 06:33:19 -0300

    feat: implement internationalization (i18n) with pt-br and en-us support
    This commit introduces internationalization support for the project. It includes:
    
    * Integration of Astro i18n route.
    * Data translations for pt-br (Brazilian Portuguese) and en-us (US English) locales.
    * Ability to switch between languages.
    
    This enables the application to be displayed in different languages, improving accessibility for a wider audience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config'
+import { defaultLocale, locales } from './src/utils/i18n'
 
 import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcy-miniportfolio.vercel.app/',
+  i18n: {
+    defaultLocale: defaultLocale,
+    locales: locales,
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   output: 'server',
   adapter: vercel(),
 })
